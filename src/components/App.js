53437afeb900b6c1140d75a1de8c67e223446b7c
Naveen Kumar <nvnkr09@gmail.com>
2023-12-28 14:37:43 +0530

    submitting code-Thu Dec 28 2023 14:37:42 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,37 @@ import './../styles/App.css';
 
 const App = () => {
 
-  const tabsData = ["Tab 1", "Tab 2", "Tab 3"];
-  const [activeTab, setActiveTab] = useState(tabsData[0]);
+  const tabsData = 
+  [
+    {
+      title:"Tab 1",
+      content:"This is content for Tab 1"
+    },
+    {
+      title:"Tab 2",
+      content:"This is content for Tab 2"
+    },
+    {
+      title:"Tab 3",
+      content:"This is content for Tab 3"
+    }
+  ];
+
+  const [activeTab, setActiveTab] = useState(tabsData[0].content);
 
   return (
     <div>
         <ul>
           {
           tabsData.map((tab)=>(
-              <li onClick={()=>setActiveTab(tab)}>
-                {tab}
+              <li onClick={()=>setActiveTab(tab.content)}>
+                {tab.title}
               </li>
             ))
           }
         </ul>
         <div>
-           <p>This is content for {activeTab}</p>
+           <p>{activeTab}</p>
         </div>
     </div>
   )
