7c930bc3778fc73d1e2fbee7274344f4cdac8b8f
Naveen Kumar <nvnkr09@gmail.com>
2023-12-28 14:42:05 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,36 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
-
-  const tabsData = 
-  [
+  const tabsData = [
     {
-      title:"Tab 1",
-      content:"This is content for Tab 1"
+      title: "Tab 1",
+      content: "This is content for Tab 1",
     },
     {
-      title:"Tab 2",
-      content:"This is content for Tab 2"
+      title: "Tab 2",
+      content: "This is content for Tab 2",
     },
     {
-      title:"Tab 3",
-      content:"This is content for Tab 3"
-    }
+      title: "Tab 3",
+      content: "This is content for Tab 3",
+    },
   ];
 
   const [activeTab, setActiveTab] = useState(tabsData[0].content);
 
   return (
     <div>
-        <ul>
-          {
-          tabsData.map((tab)=>(
-              <li onClick={()=>setActiveTab(tab.content)}>
-                {tab.title}
-              </li>
-            ))
-          }
-        </ul>
-        <div>
-           <p>{activeTab}</p>
-        </div>
+      <ul>
+        {tabsData.map((tab) => (
+          <li onClick={() => setActiveTab(tab.content)}>{tab.title}</li>
+        ))}
+      </ul>
+      <div>
+        <p>{activeTab}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
